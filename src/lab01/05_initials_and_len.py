8abc34942dd1f8eea306f17794cd8833db96acee
axasdel <axasdelka@gmail.com>
2025-09-25 13:28:14 +0300

    Изменение 5ого задания

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -2,9 +2,7 @@ bio = input('ФИО:')
 fio = bio.split()
 initials = ''.join([i[0] for i in fio]).upper()
 dlina = 0
-for l in bio:
-    dlina += 1
-    if l == ' ':
-        dlina -= 1
+for l in fio:
+    dlina += len(l)
 print(f'Инициалы:{initials}.')
-print(f'Длина (символов):{dlina}')
+print(f'Длина (символов):{dlina + 2}')
