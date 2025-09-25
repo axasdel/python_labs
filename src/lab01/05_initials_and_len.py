7436debd42ea6cad1fd25ff41261990bcf4c3a19
axasdel <axasdelka@gmail.com>
2025-09-25 13:16:38 +0300

    Изменение 5ого задания

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -4,5 +4,7 @@ initials = ''.join([i[0] for i in fio]).upper()
 dlina = 0
 for l in bio:
     dlina += 1
+    if l == ' ':
+        dlina -= 1
 print(f'Инициалы:{initials}.')
 print(f'Длина (символов):{dlina}')
---
+++ src/lab02/arrays.py
@@ -0,0 +1,7 @@
+spisok = [3, -1, 5, 5, 0]
+res = []
+def min_max(spisok):
+    mini = min(spisok)
+    maxi = max(spisok)
+    return res.append(mini)
+print(res)
