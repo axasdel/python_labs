92d4cdd56d94ac4aba3873f87193a5a986e221b3
axasdel <axasdelka@gmail.com>
2025-10-22 18:38:25 +0300

    ...

--- src/lab03/vvv.py
+++ src/lab03/vvv.py
@@ -1,4 +1,4 @@
-tokens = ["bb","aa","bb","aa","cc", 'aa']
+tokens = ["bb","aa","bb","aa","cc"]
 
 def count_freq(tokens):
     dict_token = {}
@@ -11,8 +11,16 @@ slovar = count_freq(tokens) #словарь
 print(slovar)
 
 def top_n(slovar, n):
-    res = sorted([...])[::-1]
-    return res[:n]
-    
-
-# print(top_n(slovar))
+    spisok = []
+    for key, value in slovar.items():
+        spisok.append((key, value))
+    for i in range(len(spisok)):
+        for j in range(i + 1, len(spisok)):
+            if spisok[i][0] > spisok[j][0]:
+                spisok[i], spisok[j] = spisok[j], spisok[i]
+    for i in range(len(spisok)):
+        for j in range(i + 1, len(spisok)):
+            if spisok[i][1] < spisok[j][1]:
+                spisok[i],spisok[j] = spisok[j], spisok[i]
+    return spisok[:n]
+print(top_n(slovar, 2))
