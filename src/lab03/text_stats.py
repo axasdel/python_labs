e003d55a6f40ce5ce2c7470f63698a443c17c55d
axasdel <axasdelka@gmail.com>
2025-10-30 11:53:52 +0300

    lab03

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -4,7 +4,7 @@ from lib.normalize_function import normalize
 from lib.tokenize_function import tokenize
 from lib.count_freq_top_n_function import count_freq, top_n
 
-import io
+import io #инструмент инпут аутпут
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 text = sys.stdin.read().strip()
 # text = 'мир, привет!!! привет!!'
@@ -19,6 +19,5 @@ top = top_n(slovar, 5)
 print("Всего слов:",len(tokens))
 print("Уникальных слов:",len(slovar))
 print("Топ-5:")
-print(top)
 for slova in top:
     print(slova[0], ':', slova[1])
