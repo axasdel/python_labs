d893bc98475d6b23e8411912a8cbd6536f2fb923
axasdel <axasdelka@gmail.com>
2025-11-18 22:19:01 +0300

    лаба 5. улучшение кода

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -5,6 +5,7 @@ import csv
 
 from pathlib import Path
 
+
 def json_to_csv(json_path, csv_path):
     json_newpath = Path(json_path)
     csv_newpath = Path(csv_path)
@@ -15,15 +16,19 @@ def json_to_csv(json_path, csv_path):
     with json_newpath.open('r', encoding = 'utf-8') as jf:
         json_text = json.load(jf)
 
-    empty_makers = [None, 0, '', False]
-    if json_text in empty_makers or len(json_text) == 0:
+    if json_text is None:
         raise ValueError('Файл пуст')
+    if len(json_text) == 0:
+        raise ValueError('Файл пуст')
+    if type(json_text) != list:
+        raise ValueError('JSON не список')
+    if not all(isinstance(element, dict) for element in json_text): #все ли элементы - словари
+        raise ValueError('JSON - список словарей')
     
-
-    fieldnames = list(json_text[0].keys())
+    fieldnames = list(json_text[0].keys()) #преобразование ключей 1ого словаря в заголовок для будующего csv
     with csv_newpath.open('w', encoding= 'utf-8', newline = '') as cf:
         csv_writer = csv.DictWriter(cf, fieldnames = fieldnames, extrasaction = 'ignore')
-        csv_writer.writeheader()
+        csv_writer.writeheader()                                #игнорирование лишних ключей
         csv_writer.writerows(json_text)
 
 
@@ -46,6 +51,8 @@ def csv_to_json(csv_path, json_path):
 
     with json_newpath.open('w', encoding='utf-8') as jf:
         json.dump(text, jf, ensure_ascii=False, indent = 2)
+                                               #отступы
+
 
 json_path = 'src/lab05/data/samples/people.json'
 csv_outpath = 'src/lab05/data/result/people_from_json.csv'
