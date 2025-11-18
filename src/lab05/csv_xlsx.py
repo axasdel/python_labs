9f3db10154a5d02eebd71e5a9db9eae3ba1033e9
axasdel <axasdelka@gmail.com>
2025-11-18 22:02:46 +0300

    лаба 5

--- src/lab05/csv_xlsx.py
+++ src/lab05/csv_xlsx.py
@@ -3,7 +3,8 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
 from pathlib import Path
-
+import sys
+sys.path.append(r'C:\Users\user\Desktop\python_labs\src')
 
 def csv_to_xlsx(csv_path_1, xlsx_path_2):
     csv_newpath = Path(csv_path_1)
@@ -12,29 +13,31 @@ def csv_to_xlsx(csv_path_1, xlsx_path_2):
     if not csv_newpath.exists():
         raise FileNotFoundError('Файла csv не существует')
     
-    workbook = Workbook()
-    work_sheet = workbook.active
+    workbook = Workbook() #создание книги эксель
+    work_sheet = workbook.active #активный лист
     work_sheet.title = 'Sheet1'
 
-    collumn_width = {}
-    for collumn_ind, width in collumn_width.items():
-        letter = get_column_letter(collumn_ind)
-        work_sheet.column_dimensions[letter].width = (width + 2) if \
-        (width + 2) >= 8 else 8
+    column_width = {} #для макс ширины столбца
 
     with csv_newpath.open('r', encoding='utf-8') as cf:
         csv_reading = csv.reader(cf)
         for row in csv_reading:
             work_sheet.append(row)
-            for collumn_ind, value in enumerate(row, start = 1):
-                cell_length = len(value)
-                if cell_length > collumn_width.get(collumn_ind, 0):
-                    collumn_width[collumn_ind] = cell_length
+            for column_ind, value in enumerate(row, start = 1): #enumerate для получения (индекс, значение)
+                cell_length = len(value) #длина значения
+                if cell_length > column_width.get(column_ind, 0): #случай, если длина значения > текущей макс ширины
+                    column_width[column_ind] = cell_length
+
+    for column_ind, width in column_width.items(): #для индекса, ширины в словаре
+        letter = get_column_letter(column_ind) #исходя из индекса, присваеваем столбцам экселевские буквы
+        work_sheet.column_dimensions[letter].width = (width + 2) if \
+        (width + 2) >= 8 else 8                     #по 1 отступу с обеих сторон
 
     workbook.save(xlsx_newpath)
 
 
-csv_path = 'lab05/data/samples/people.csv'
-xlsx_path = 'lab05/data/result/res_people.csv'
-csv_to_xlsx(csv_path, xlsx_path)
+if __name__ == "__main__": #запуск напрямую
+    csv_path = 'src/lab05/data/samples/people.csv'
+    xlsx_path = 'src/lab05/data/result/res_people.xlsx'
+    csv_to_xlsx(csv_path, xlsx_path)
 
